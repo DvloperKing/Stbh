3ee47853a80c6776f04225ac186bd9919adcdeca
Eliezer HG <128151447+EliezerHg@users.noreply.github.com>
2025-02-19 13:42:40 -0600

    Create git repository

---
+++ pages/index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/icon_stbh.png">
+  <title>
+    STBH | Procóro García Hernández
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="../assets/css/soft-ui-dashboard.css?v=1.0.8" rel="stylesheet" />
+  <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
+  <style>
+    .move-up {
+      margin-top: -50px; /* Ajusta este valor según sea necesario */
+    }
+
+    .image-container {
+      position: absolute;
+      top: 125px;
+      right: 0;
+      bottom: 0;
+      display: flex;
+      align-items: center;
+    }
+
+    .oblique-image {
+    width: 100%;
+    height: 370px;
+    }
+    .logos-container {
+      background-color: #fff; /* Color de fondo */
+      padding: 12px; /* Espacio interno */
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Sombra */
+    }
+    .logos img.rounded {
+      width: 100px; /* Tamaño de las imágenes */
+      margin-right: 20px; /* Espacio entre las imágenes */
+    }
+    .logos-container {
+      display: flex;
+      justify-content: center; /* Centra horizontalmente */
+      align-items: center; /* Centra verticalmente */
+      background-color: #fff; /* Color de fondo */
+      padding: 12px; /* Espacio interno */
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Sombra */
+    }
+    .logos {
+      display: flex;
+      justify-content: center; /* Centra horizontalmente */
+      align-items: center; /* Centra verticalmente */
+    }
+
+    .logos img.rounded {
+      width: 100px; /* Tamaño de las imágenes */
+      margin-right: 20px; /* Espacio entre las imágenes */
+    }
+  </style>
+</head>
+    
+<body class="">
+<div class="logos-container">
+    <!-- Imágenes de la clase .logos -->
+    <div class="logos">
+      <img class="rounded" src="../assets/img/cnbm.png"  alt="CNBM Logo"  style="width: 300px;">
+      <img class="rounded" src="../assets/img/CRBH2.png"  alt="CRBH Logo"  style="width: 100px;">
+      <img class="rounded" src="../assets/img/stbm.png"  alt="STBM Logo"  style="width: 300px;">
+    </div>
+  </div>
+  <div class="container position-sticky z-index-sticky top-0">
+    <div class="row">
+      <div class="col-12">
+      </div>
+    </div>
+  </div>
+  <main class="main-content mt-0">
+    <section>
+      <div class="page-header min-vh-75 move-up">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
+              <div class="card card-plain mt-8" >
+                <div class="card-header pb-0 text-left bg-transparent">
+                  <h3 class="font-weight-bolder text-info text-gradient text-center">Bienvenido!</h3>
+                  <p class="mb-0 text-center">Seleccione su Perfil</p>
+                </div>
+                <div class="card-body">
+                  <form role="form">
+                    <div class="text-center">
+                      <button onclick="location.href='./admin.php'" Id="btn__Admin" class="btn bg-gradient-info w-100 mt-4 mb-0">Administración</button>
+                    </div>
+                    <div class="text-center">
+                      <button onclick="location.href='./docente.php'" class="btn bg-gradient-info w-100 mt-4 mb-0">Docente</button>
+                    </div>
+                    <div class="text-center">
+                      <button onclick="location.href='./alumno.php'" class="btn bg-gradient-info w-100 mt-4 mb-0">Alumno</button>
+                    </div>
+                  </form>
+                </div> 
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="image-container">
+                <img class="oblique-image" src="../assets/img/curved-images/logo2.png" alt="Logo">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+  <!-- -------- FOOTER ------- -->
+  <footer class="footer py-5">
+    <div class="container">        
+      <div class="row">
+        <div class="col-8 mx-auto text-center mt-1">
+          <p class="mb-0 text-secondary">
+           STBH © <script>
+              document.write(new Date().getFullYear())
+            </script>  | Todos los derechos Reservados
+
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- -------- END FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
+  <!--   Core JS Files   -->
+  <script src="../assets/js/core/popper.min.js"></script>
+  <script src="../assets/js/core/bootstrap.min.js"></script>
+  <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+  <!-- <script src="../assets/js/soft-ui-dashboard.min.js?v=1.0.7"></script> -->
+
+</body>
+
+</html>
