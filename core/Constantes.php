afaf6bd54d12f79fcc898496f00ea9737f43943a
Eliezer HG <128151447+EliezerHg@users.noreply.github.com>
2025-02-21 13:00:07 -0600

    Constantes.php

--- core/Constantes.php
+++ core/Constantes.php
@@ -1,11 +1,11 @@
 <?php
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
  
-define('HDND_HOST', 'localhost');# localhost
+define('HDND_HOST', 'localhost');
 define('HDND_USER', 'root');
 define('HDND_PASS', '12345');
-define('HDND_DB_NAME', 'Proyecto_IS');
-define("MYSQL_PORT", 3306); #2083
+define('HDND_DB_NAME', 'stbh');
+define("MYSQL_PORT", 3306); 
 session_start();
 if(!isset($_SESSION['token'])){
     $u= uniqid(rand());
