be5f9fae26a4a462bfb0d8be1faef94ed645e6d1
Eliezer HG <eliezerh2508@gmail.com>
2025-03-03 09:36:12 -0600

    Actualizacion

--- pages/docente.php
+++ pages/docente.php
@@ -91,7 +91,7 @@
         <div class="collapse navbar-collapse" id="navbarScroll">
             <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <!-- <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Calificaciones
                     </a>
                     <ul class="dropdown-menu">
@@ -117,7 +117,7 @@
                         <li><a class="dropdown-item" href="#">Revisar Tarea</a></li>
                     </ul>
                 </li>
-            </ul>
+            </ul> -->
         </div>
     </div>
 </nav>
